7dba8c0a00b2aa8feda1c06b56ba652371a94fe0
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-04-10 16:59:30 +0530

    niraj 2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type"],
     // credentials: true,
   })
 );
