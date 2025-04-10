400e133c7ceae1074f19c2d63ee1a6f670e1a5ba
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-04-10 16:57:45 +0530

    credentails

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // credentials: true,
   })
 );
 
